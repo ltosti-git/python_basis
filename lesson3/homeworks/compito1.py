4e887951dd370b50a2d0203a94a9a2320667a3a7
ltosti <ltosti@centos-stream.pihole>
2022-07-13 17:39:35 +0200

    test: Updated first homework

--- lesson3/homeworks/compito1.py
+++ lesson3/homeworks/compito1.py
@@ -1,3 +1,8 @@
+'''
+Creare un programma che prenda in input dall'utente una serie di parole o punteggiatura (sono ammessi solo . , ! e ?), una per volta. L'inserzione si deve interrompere quando l'utente inserisce la stringa "stop".
+
+Finita l'inserzione si stampi la serie di frasi risultante, andando a capo dopo ogni frase (ossia dopo ogni ., ! o ?). Assicurarsi che non vi siano spazi prima della , e che le maiuscole siano corrette.
+'''
 print("\n____________________________________________\n\n")
 print("compito 1\n___________________\n")
 
@@ -15,7 +20,7 @@ while "stop" not in list_txt:
             join=" ".join(list_txt)
             if "stop" in join:
                 index_stop=join.find("stop")
-                join=join[:index_stop-1]
+                join=join[:index_stop-1]+" "
                 for c in join:
                     if ("," in c or "." in c or "!" in c or "?" in c):
                         c=c.strip()
