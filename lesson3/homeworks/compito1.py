b400f2dd7555264c557d497e3a5c93ceab6d6c37
ltosti <ltosti@centos-stream.pihole>
2022-07-14 17:34:43 +0200

    test: Updated first homework and testing

--- lesson3/homeworks/compito1.py
+++ lesson3/homeworks/compito1.py
@@ -2,8 +2,16 @@
 Creare un programma che prenda in input dall'utente una serie di parole o punteggiatura (sono ammessi solo . , ! e ?), una per volta. L'inserzione si deve interrompere quando l'utente inserisce la stringa "stop".
 
 Finita l'inserzione si stampi la serie di frasi risultante, andando a capo dopo ogni frase (ossia dopo ogni ., ! o ?). Assicurarsi che non vi siano spazi prima della , e che le maiuscole siano corrette.
+
+Riportare poi un po' di statistiche in merito al testo, ed in particolare:
+
+Numero di parole ricevute (non si contino i segni di interpunzione)
+Numero di segni di interpunzione
+Numero di frasi
+Il carattere più frequente
+Il carattere meno frequente
 '''
-print("\n____________________________________________\n\n")
+print("____________________________________________\n")
 print("compito 1\n___________________\n")
 
 list_txt=[]
@@ -11,23 +19,31 @@ list_txt=[]
 while "stop" not in list_txt:
     txt=input("Please, insert a single word or \", ! ?\" characters: ")
     txt_strip=txt.strip()
-#memo: quale controllo implementare per ammettere solo la punteggiatura ', . ! ?' e anche solo testo a-z/A-Z??
+    #memo: quale controllo implementare per ammettere solo la punteggiatura ', . ! ?' e anche solo testo a-z/A-Z??
     if txt_strip!="":
         if txt_strip.find(" ")!=-1:
             print(f"error! space detected in {txt_strip}. Remind: insert just ONE word")
         else:
             list_txt.append(txt_strip)
-            join=" ".join(list_txt)
-            if "stop" in join:
-                index_stop=join.find("stop")
-                join=join[:index_stop-1]+" "
-                for c in join:
-                    if ("," in c or "." in c or "!" in c or "?" in c):
+            join_list_txt=" ".join(list_txt)
+            if "stop" in join_list_txt:
+                index_stop=join_list_txt.find("stop")
+                join_list_txt=join_list_txt[:index_stop-1]+" "
+                join_list_txt=join_list_txt.capitalize()
+                
+                for c in join_list_txt:
+                    
+                    if ("." in c or "!" in c or "?" in c):                       
                         c=c.strip()
-                        join=join.replace(f" {c} ",f"{c}\n")
-#memo: capitalize delle lettere dopo la punteggiatura?
+                        find_c=join_list_txt.find(c)                        
+                        join_list_txt=join_list_txt.replace(f" {c} ",f"{c}\n")
+                        #memo: capitalize delle lettere dopo la punteggiatura?
+                    if ("," in c):
+                        c=c.strip()                        
+                        join_list_txt=join_list_txt.replace(f" {c} ",f"{c} ")
+                               
     else:
         print("Empty!")
 
     
-print(join)
+print(join_list_txt)
--- lesson3/test.py
+++ lesson3/test.py
@@ -10,9 +10,9 @@
 # print(x)
 
 list_txt=[]
-list_pr=["ciao","come","stai",",","tutto","bene","?","si","grazie"]
-list_pr=list_pr[:-1]
-print(list_pr)
+# list_pr=["ciao","come","stai",",","tutto","bene","?","si","grazie"]
+# list_pr=list_pr[:-1]
+# print(list_pr)
 
 while "stop" not in list_txt:
     txt=input("Please, insert a single word or \", ! ?\" characters: ")
@@ -23,23 +23,53 @@ while "stop" not in list_txt:
             print(f"error! space detected in {txt_strip}. Remind: insert just ONE word")
         else:
             list_txt.append(txt_strip)
-            join=" ".join(list_txt)
-            if "stop" in join:
-                index_stop=join.find("stop")
-                join=join[:index_stop-1]+" "
-                for c in join:
-                    if ("," in c or "." in c or "!" in c or "?" in c):
-                        join=join.replace(f" {c} ",f"{c}\n")
-                        # if ("," not in c):
-                        #     print(c)                    
+            join_list_txt=" ".join(list_txt)
+            if "stop" in join_list_txt:
+                index_stop=join_list_txt.find("stop")
+                join_list_txt=join_list_txt[:index_stop-1]+" "
+                join_list_txt=join_list_txt.capitalize()
+                
+                for c in join_list_txt:
+                    
+                    if ("." in c or "!" in c or "?" in c):                       
+                        c=c.strip()
+                        find_c=join_list_txt.find(c)                        
+                        join_list_txt=join_list_txt.replace(f" {c} ",f"{c}\n")
+                        join_list_txt=join_list_txt.replace(f"{join_list_txt[find_c+2]}\n",f"{join_list_txt[find_c+2].capitalize()}\n")
+                        
+                        
+                    if ("," in c):
+                        c=c.strip()                        
+                        join_list_txt=join_list_txt.replace(f" {c} ",f"{c} ")
+                               
     else:
         print("Empty!")
 
     
-print(join)
+print(join_list_txt)
 
-print("\n Inserts completed! \n Now, some useful info:")
-for c in join:
-    if ("," not in c or "." not in c or "!" not in c or "?" not in c or " " not in c):
-        total=sum(len(c))
-print(total)
+# print("\n Inserts completed! \n Now, some useful info:")
+
+# join_list_txt_dict={}
+# join_list_txt=join_list_txt.replace("\n","")
+# join_list_txt=join_list_txt.replace(",","")
+# print(join_list_txt)
+# for one_char in join_list_txt:
+#     if ("," in one_char or " " in one_char or "." in one_char or "!" in 
+#     one_char or "?" in one_char or " " in one_char):
+#         join_list_txt=join_list_txt.replace(f"{one_char}",f"")
+
+#     if one_char in join_list_txt_dict.keys():
+#         join_list_txt_dict[one_char]+=1
+#     else:
+#         join_list_txt_dict[one_char]=1
+# print(join_list_txt_dict)
+
+# prova=["zero",",","uno"]
+# for i in range(len(prova)):
+#     if "," in prova[i]:
+#         #print(prova[i])
+#         prova[i]=prova[i].replace(prova[i],"")
+#         #print(asd)
+#     print(prova[i])
+    
