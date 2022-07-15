89f81734c31b9266c8377b64f99f2c1ec8851b8f
ltosti <ltosti@centos-stream.pihole>
2022-07-15 18:52:14 +0200

    test: testing lesson3/first homework

--- lesson3/test.py
+++ lesson3/test.py
@@ -9,6 +9,9 @@
 # x="-".join(txt2)
 # print(x)
 
+from string import punctuation
+
+
 list_txt=[]
 # list_pr=["ciao","come","stai",",","tutto","bene","?","si","grazie"]
 # list_pr=list_pr[:-1]
@@ -32,8 +35,6 @@ while "stop" not in list_txt:
                         if list_txt[i+1]!="stop":
                             list_txt[i+1]=list_txt[i+1].capitalize()   
 
-                print(list_txt)
-
                 join_list_txt=" ".join(list_txt)
                 index_stop=join_list_txt.find("stop")
                 join_list_txt=join_list_txt[:index_stop-1]+" "                
@@ -53,31 +54,69 @@ while "stop" not in list_txt:
         print("Empty!")
 
     
-print(join_list_txt)
-
-
+#print(join_list_txt)
 print("\n Inserts completed! \n Now, some useful info:")
 
 join_list_txt_dict={}
+punctuation_dict={}
+
 join_list_txt=join_list_txt.replace("\n","")
-join_list_txt=join_list_txt.replace(",","")
-print(join_list_txt)
-for one_char in join_list_txt:
-    if ("," in one_char or " " in one_char or "." in one_char or "!" in 
-    one_char or "?" in one_char or " " in one_char):
-        join_list_txt=join_list_txt.replace(f"{one_char}",f"")
+join_list_txt=join_list_txt.replace(" ","")
+#print(join_list_txt)
 
-    if one_char in join_list_txt_dict.keys():
-        join_list_txt_dict[one_char]+=1
+for c in join_list_txt:
+    c=c.lower()
+    if not ("," in c or "." in c or "!" in c or "?" in c):        
+        if c in join_list_txt_dict.keys():
+            join_list_txt_dict[c]+=1
+        else:
+            join_list_txt_dict[c]=1
     else:
-        join_list_txt_dict[one_char]=1
-print(join_list_txt_dict)
+        if c in punctuation_dict.keys():
+            punctuation_dict[c]+=1
+        else:
+            punctuation_dict[c]=1
 
-prova=["zero",",","uno"]
-for i in range(len(prova)):
-    if "," in prova[i]:
-        #print(prova[i])
-        prova[i]=prova[i].replace(prova[i],"")
-        #print(asd)
-    print(prova[i])
+words_list=[]
+puctuation_list=[]
+
+max=0
+min=0
+pref_char=""
+min_char=""
+for word,word_count in join_list_txt_dict.items():
+    if word_count > max:
+        max=word_count
+        pref_char=word
+    if word_count < min:
+        min=word_count
+        min_char=word
+
+print(f"the preferred character is: {pref_char} with {max} occurrences")
+print(f"the less used character is: {min_char} with {min} occurrences")
+
+max=0
+min=0
+pref_punct=""
+min_punct=""
+for punct,punct_count in punctuation_dict.items():
+    if punct_count > max:
+        max=punct_count
+        pref_punct=punct
+    if punct_count < min:
+        min=punct_count
+        min_punct=punct
+
+print(f"the preferred punctuation sign is: {pref_punct} with {max} occurrences")
+print(f"the less used punctuation sign is: {min_punct} with {min} occurrences")
+
+
+
+
+print(join_list_txt_dict)
+print(punctuation_dict)
+
+
+
+
     
