7c6d66d61fff202a03a6ace3ebf41b7a5749a1b9
ltosti <ltosti@centos-stream.pihole>
2022-07-20 18:54:21 +0200

    test: Updated second homework and testing

--- lesson3/homeworks/compito2.py
+++ lesson3/homeworks/compito2.py
@@ -1,11 +1,24 @@
 def main():
     print("calls other functions")
-    list_txt=["ciao","come","stai",",","tutto","bene","?","si","grazie",".","tu","?","non","male",".","stop"]
-    #list_txt=["bene","?","si",".","tu","?","male"]
-    rev(list_txt)
+    list_txt=["ciao","come","stai",",","tutto","bene","?","si","grazie",".","tu","?","non","male",".","ciao","come","stai",",","tutto","bene","?","si","grazie",".","tu","?","non","male",".","stop"]
+    res_rev=rev(list_txt)
+    print(f"reverse list is:\n{res_rev}")
 
-    num=-2
-    print(is_positive(num))
+    res_is_positive=is_positive(3202)
+
+    if res_is_positive == True or res_is_positive == False:
+        print("is_positive function working out correctly")
+    else:
+        print("is_positive function not properly working")
+    
+    nList=[-5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6]
+
+    result_no_duplicate=no_duplicate(nList)
+    print(result_no_duplicate)
+    
+    number=5
+    result_fact=factorial(number)
+    print(f"Factorial of {number} is: {result_fact}")
 
 
 def rev(list):
@@ -13,23 +26,41 @@ def rev(list):
     length_list=len(list)-1
     for i in range(length_list,-1,-1):
         rev_list.append(list[i])
-    return print(rev_list)
+    return rev_list
 
 
 def is_positive(n):
     
     if n<0:
-        # print(f"Number {i} is negative!")
-        return False       
+        print(f"Number {n} is negative!")       
+        return False   
     else:
-        # print(f"Number {i} is positive!")       
-        return True
+        print(f"Number {n} is positive!")
+        return True       
+
+def no_duplicate(list):
+    set_list=set()
+    for i in range(len(list)):
+        set_list.add(list[i])
+    return set_list
+
+# def factorial(n):
+#     fact=1
+#     for i in range(n,0,-1):
+#         fact*=i        
+#     return fact
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
 
 
 
 if __name__=="__main__":
-    try:
-        print("main foo")
-        main()
-    except:
-        print("troubles")
+    # try:
+    print("main foo")
+    main()
+    # except:
+    #     print("troubles")
--- lesson3/test2.py
+++ lesson3/test2.py
@@ -1,23 +1,49 @@
-list_txt=["ciao","come","stai",",","tutto","bene","?","si","grazie",".","tu","?","non","male",".","stop"]
+#list_txt=["ciao","come","stai",",","tutto","bene","?","si","grazie",".","tu","?","non","male",".","stop"]
 #list_txt=["bene","?","si",".","tu","?","male"]
+
+nList=[-5, -4, -4, -3, -3, -2, -2, -1, -1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6]
+
+## Generate a list with numbers in a foo
+# maxRange=6
+# def create_list(num):
+#     nlist=[]
+#     for i in range(-5,num):
+#         nlist.append(i)
+#         nlist.append(i+1)
+#     return nlist
+# print(create_list(maxRange))
 
 def rev(list):
     rev_list=[]
     length_list=len(list)-1
     for i in range(length_list,-1,-1):
         rev_list.append(list[i])
-    return print(rev_list)
+    return rev_list
 
-#rev(list_txt)
+res_rev=rev(nList)
+print(res_rev)
 
-for i in range(-10,10):
+def is_positive(n):
+    for i in range(len(n)):
+        #print(n[i])
+        if n[i]<0:
+            print(f"Number {n[i]} is negative!")
+            #return False
+        else:
+            print(f"Number {n[i]} is positive!")       
+            #return True
+
+res_is_positive=is_positive(nList)
+print(res_is_positive)
+
+# for i in range(-10,10):
     
-    if i<0:
-        # print(f"Number {i} is negative!")
-        False       
-    else:
-        # print(f"Number {i} is positive!")       
-        True
+#     if i<0:
+#         # print(f"Number {i} is negative!")
+#         False       
+#     else:
+#         # print(f"Number {i} is positive!")       
+#         True
 
 # x = 50
 # y = 20
@@ -25,15 +51,36 @@ for i in range(-10,10):
 # print(x % y)
 
 
+def no_duplicate(list):
+    set_list=set()
+    for i in range(len(list)):
+        set_list.add(list[i])
+    return set_list
+    
+result_no_duplicate=no_duplicate(nList)
+print(result_no_duplicate)
 
 
 
+number=10
+# def factorial(n):
+#     fact=1
+#     for i in range(n,0,-1):
+#         fact*=i        
+#     print(fact)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+result_fact=factorial(number)
+print(result_fact)
 
 
-
-
-
-
+def is_palindrome(string):
+    print(string)
 
 
 
