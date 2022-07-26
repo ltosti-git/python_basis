0b3b954f533902c9120897269ea50bcaaaef771a
ltosti <ltosti@centos-stream.pihole>
2022-07-26 17:22:59 +0200

    test: Created lesson4/compito1.py and testing

---
+++ lesson4/testData.py
@@ -0,0 +1,8 @@
+# file01=open("data/file_01.txt","r")
+# print(file01.read(3))
+# file01.close()
+
+# lines=[]
+# with open('data/file_01.txt','r') as file01:
+#     lines=file01.readlines()
+# print(lines[3])
