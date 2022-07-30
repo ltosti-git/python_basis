32d10d2dd7dd3a6f999f369ab70b058bbf833dbd
ltosti <ltosti@centos-stream.pihole>
2022-07-30 19:31:11 +0200

    test: Updated lesson4/compito4

--- lesson4/homeworks/compito4.py
+++ lesson4/homeworks/compito4.py
@@ -1,10 +1,11 @@
-import numpy as np
+#import numpy as np
 
 f1_path='lesson4/data/file_compito4.txt'
 f2_path='lesson4/data/file_compito4_matrice.txt'
 
 l1=[]
 l2=[]
+matrix=[]
 
 with open(f1_path,'r') as f_01:
     line1=f_01.readline()
@@ -15,24 +16,31 @@ with open(f1_path,'r') as f_01:
 
 #print(l1,l2)
 
-matrix=np.zeros((len(l1), len(l2)), dtype=np.int16)
+#matrix=np.zeros((len(l1), len(l2)), dtype=np.int16)
 #print(matrix.dtype)
 # matrix[0][0]=(l1[0]+l2[0])
 # matrix[1][1]=(l1[1]+l2[1])
 # matrix[-1][-1]=(l1[2]+l2[2])
 
-for i in range(len(l1)):
-    for j in range(len(l1)):
-        if i == j:
-            matrix[i][i]=l1[i]+l2[i]
+# for i in range(len(l1)):
+#     for j in range(len(l1)):
+#         if i == j:
+#             matrix[i][i]=l1[i]+l2[i]
 
+for i in range(len(l1)):
+    line=[0] * len(l1)
+    line[i]=l1[i]+l2[i]
+    matrix.append(line)
+#print(matrix)
+
+# for line in matrix:
+#     for x in line:
+#         line=str(x)
+#         print(line)
 
 with open(f2_path,'w') as f_02:
     for line in matrix:
-        line=str(line)
-        line=line.strip("[").strip("]")
-
-        f_02.write(line)
+        f_02.write(" ".join([str(x) for x in line]))
         f_02.write('\n')
 
 #print(matrix)
