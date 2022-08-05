cceb717ef8e3e53fdf35c3677da68bd01f164991
ltosti <ltosti@centos-stream.pihole>
2022-08-05 13:26:44 +0200

    main: Updated lesson5 compito04

--- lesson5/compiti/compito04.py
+++ lesson5/compiti/compito04.py
@@ -2,10 +2,10 @@ import json, datetime, requests
 
 PROVINCE_PATH="lesson5/data/province.json"
 api_meteo="https://api.open-meteo.com/v1/"
-call="forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&hourly=apparent_temperature&timezone=CET"
+#call="forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&hourly=apparent_temperature&timezone=CET"
 hour = int(datetime.datetime.now().strftime("%H"))
-#print(hours)
 
+# get user input and check if it exists in loaded json file
 try:
     province_name=input("Digitare il nome della provincia: ")
    
@@ -15,17 +15,18 @@ try:
     if province_name.title() not in p_json.keys():
         raise Exception("Provincia non trovata!")
 
+    # loop in json, checking the province name and getting lat and lon
     for k,v in p_json.items():
-        #print(k)
+  
         if province_name.title() in k: 
             print("Provincia trovata!")
             lat=v['lat']
             lon=v['lon']
-            #print(lat,lon)
-            res=requests.get(f"{api_meteo}forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&hourly=apparent_temperature&timezone=CET") #
-            # print(res)
-            api_call=res.json()
-            #print(api_call)
+
+            # request to api with obtained datas and parsing response in json
+            api_call=requests.get(f"{api_meteo}forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&hourly=apparent_temperature&timezone=CET").json()
+
+            # print requested infos
             time=api_call["hourly"]["time"][hour]
             temp=api_call["hourly"]["temperature_2m"][hour]
             app_temp=api_call["hourly"]["apparent_temperature"][hour]
