f0c079485ef53aec96ff3d448f3baf2fac8e2988
ltosti <ltosti@centos-stream.pihole>
2022-08-05 11:59:37 +0200

    main: Updated lesson5 compito03

--- lesson5/compiti/compito03.py
+++ lesson5/compiti/compito03.py
@@ -1,13 +1,12 @@
 from prettytable import PrettyTable
 import requests
+
 # do the requests and parsing in json
 url_todos='https://jsonplaceholder.typicode.com/todos/'
 url_users='https://jsonplaceholder.typicode.com/users/'
 
-todos=requests.get(url_todos)
-todos_json=todos.json()
-users=requests.get(url_users)
-users_json=users.json()
+todos_json=requests.get(url_todos).json()
+users_json=requests.get(url_users).json()
 
 # get the datas and instantiate a table for each user 
 for user in users_json:
@@ -15,6 +14,7 @@ for user in users_json:
     name=user['name']
     table=PrettyTable()
     table.field_names=[name,"TODO"]
+
     # get the todos and check the IDs to add rows for each user table
     for todo in todos_json:
         userId=todo['userId']
@@ -26,6 +26,7 @@ for user in users_json:
         
         if id==userId and completed==False:
             table.add_row([title,"X"])        
-        
+    
+    #print all user tables 
     print(table)
      
