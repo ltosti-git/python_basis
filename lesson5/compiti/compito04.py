d9c5fe76cc7e289139f3c5c890b1572fa9f65e8f
ltosti <ltosti@centos-stream.pihole>
2022-08-05 13:17:03 +0200

    main: Updated lesson5 compito04

--- lesson5/compiti/compito04.py
+++ lesson5/compiti/compito04.py
@@ -2,8 +2,8 @@ import json, datetime, requests
 
 PROVINCE_PATH="lesson5/data/province.json"
 api_meteo="https://api.open-meteo.com/v1/"
-call="forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-hours = datetime.datetime.now().strftime("%H")
+call="forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&hourly=apparent_temperature&timezone=CET"
+hour = int(datetime.datetime.now().strftime("%H"))
 #print(hours)
 
 try:
@@ -21,11 +21,16 @@ try:
             print("Provincia trovata!")
             lat=v['lat']
             lon=v['lon']
-            res=requests.get(f"{api_meteo}forecast?latitude={lat}&longitude={lon}&hourly={hours}")
+            #print(lat,lon)
+            res=requests.get(f"{api_meteo}forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&hourly=apparent_temperature&timezone=CET") #
             # print(res)
-
             api_call=res.json()
-            print(api_call)
+            #print(api_call)
+            time=api_call["hourly"]["time"][hour]
+            temp=api_call["hourly"]["temperature_2m"][hour]
+            app_temp=api_call["hourly"]["apparent_temperature"][hour]
+            print(f"La temperatura alle ore {hour}: {temp}\nLa temperatura percepita alle ore {hour}: {app_temp}")
+           
 
 except Exception as exception:
     print(exception)
