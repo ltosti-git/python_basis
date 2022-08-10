3584d64e815ca77cb47bcdfa0ef227be87c2b3d1
ltosti <ltosti@centos-stream.pihole>
2022-08-10 17:51:55 +0200

    main: Updated lesson6 testing

--- lesson6/test2.py
+++ lesson6/test2.py
@@ -2,46 +2,53 @@
 Modificare il Compito 1 in modo da chiedere di indovinare chi ha più fan e non chi ha più album. Aggiungere anche nuovi artisti e testare che le chiamate API funzionino.
 '''
 
-import json, requests, random
+# import json, requests, random
 
-def get_albums(id):
-    api_url="https://api.deezer.com/artist"
-    response=requests.get(f"{api_url}/{id}").json()
-    album_nr=response['nb_album']
-    return album_nr
+# def get_albums(id):
+#     api_url="https://api.deezer.com/artist"
+#     response=requests.get(f"{api_url}/{id}").json()
+#     album_nr=response['nb_album']
+#     return album_nr
     
-ARTISTS_PATH='lesson6/data/artists.json'
+# ARTISTS_PATH='lesson6/data/artists.json'
 
-with open(ARTISTS_PATH) as file:
-    artists_dict=json.load(file)
-    artists_name=artists_dict.keys()
-keep_asking="Y"
+# with open(ARTISTS_PATH) as file:
+#     artists_dict=json.load(file)
+#     artists_name=artists_dict.keys()
+# keep_asking="Y"
 
-while keep_asking.upper()=="Y":
-    two_artists=random.sample(artists_name, 2)
+# while keep_asking.upper()=="Y":
+#     two_artists=random.sample(artists_name, 2)
     
-    try:
-        answer=input(f"Which has released the most albums between {two_artists[0]} (1) e {two_artists[1]} (2)? ")
+#     try:
+#         answer=input(f"Which has released the most albums between {two_artists[0]} (1) e {two_artists[1]} (2)? ")
         
-        if answer not in ["1","2"]:                
-            raise Exception("You must digit just number 1 or number 2!")
+#         if answer not in ["1","2"]:                
+#             raise Exception("You must digit just number 1 or number 2!")
             
-        artist_id1=artists_dict[two_artists[0]]
-        artist_id2=artists_dict[two_artists[1]]            
+#         artist_id1=artists_dict[two_artists[0]]
+#         artist_id2=artists_dict[two_artists[1]]            
         
-        artist01_albums=get_albums(artist_id1)
-        artist02_albums=get_albums(artist_id2)
+#         artist01_albums=get_albums(artist_id1)
+#         artist02_albums=get_albums(artist_id2)
 
-        solution="1" if artist01_albums > artist02_albums else "2" 
+#         solution="1" if artist01_albums > artist02_albums else "2" 
         
-        feedback="Correct!" if answer==solution else "Not correct!"
+#         feedback="Correct!" if answer==solution else "Not correct!"
 
-        feedback+=f" First artist {two_artists[0]} have {artist01_albums}, while second one {two_artists[1]} have {artist02_albums}.\nDo you wanna continue (Y/n)?"
+#         feedback+=f" First artist {two_artists[0]} have {artist01_albums}, while second one {two_artists[1]} have {artist02_albums}.\nDo you wanna continue (Y/n)?"
 
-        keep_asking=input(feedback)
+#         keep_asking=input(feedback)
 
-    except ValueError as type_error:
-        print("You must digit an integer!")
-    except Exception as exception:
-        print(exception)
-    
+#     except ValueError as type_error:
+#         print("You must digit an integer!")
+#     except Exception as exception:
+#         print(exception)
+
+def doubler(l):
+	for i in rang(len(l)):
+		l[i] = l[i]*2
+	
+l1 = [1,2,3]
+doubler(l)
+#print(l1)
