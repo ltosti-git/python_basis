5ff3182564044322d7c065b5e58b08d8dddc220a
ltosti <ltosti@centos-stream.pihole>
2022-08-25 16:25:23 +0200

    main lesson7 homework01 testing

--- lesson7/test.py
+++ lesson7/test.py
@@ -16,19 +16,33 @@ months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augu
 
 sections=death_by_year.sections
 
-def print_year(sections):
+def print_year(sections, level=0):
     years=[section.title for section in sections]
     years=years[:-1]
-
+    secs=[]
     for year in years:
-        if int(year) >= 1990 and int(year) != 2022:
-            url01=wiki_wiki.page(f'{year}#Deaths')
-            print(url01.sections)
+        #if int(year) >= 1990 and int(year) != 2022:
+        if int(year) == 1991:
+            #url01=wiki_wiki.page(f'{year}#Deaths')
+            #print(url01.sections)
             #print(f'{year}#Deaths')
 
             for month in months:
-                #url02=wiki_wiki.page(f'Deaths_in_{month}_{year}')
-                print(f'Deaths_in_{month}_{year}')
+                url02=wiki_wiki.page(f'Deaths_in_{month}_{year}')
+                #print(f'Deaths_in_{month}_{year}')
+                #print(url02.sections)
+                for s in url02.sections:
+                    #print("%s: %s - %s" % ("*" * (level+1), s.title, s.text[0:40]))
+                    secs.append(s)
+                    print(secs)
+                    for sub in secs:
+                        print("%s: %s - %s" % ("*" * (level+1), sub.title, sub.text[0:40]))
+
+                    
+                
+
 
 
 print_year(sections)
+
+dictionary={"Sergio Corbucci":["1 December 1990", 63], "Sergio Corbucci":["1 December 1990", 63]}
